80617228e33f3f527e58b8149cffcdeaa3e5ac0d
Raphael6842 <162901737+Raphael6842@users.noreply.github.com>
2025-05-24 09:37:49 -0300

    impressão dos tipos de dados básicos no php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+echo "Hello World"; // strings valores 
+
+echo "<br>";
+
+echo 2025; // int
+echo "<br>";
+echo 10.57; // float
+
+echo "<br>";
+var_dump(false); // bool - verdadeiro e falso
+echo "<br>";
+
+var_dump(null); // null 
+
+echo "<br>";
+var_dump ([
+    "João",
+    2025,
+    0
+]); // array
+
