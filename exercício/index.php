2bc27c254e3176f7155e7c9a8d06f1336e31c7d0
Raphael6842 <162901737+Raphael6842@users.noreply.github.com>
2025-05-30 19:22:00 -0300

    Exercício-Boletin-completo

--- exercício/index.php
+++ exercício/index.php
@@ -8,31 +8,125 @@
 <body>
     <h1>Seu boletin</h1>
     <?php
-        $nome = "João";
-        $nota = "8";
-
-
-        if ($nota >= 1 && $nota < 4) {
-            $situacao = " PÉSSIMA";
-            $mensagem = "Sua nota é" . $situacao;
+        
+        $nota1 = 7.2;
+        $nota2 = 6;
+        $nota3 = 4.5;
+        $nota4 = 8;
+        
+        
+        if ($nota1 >= 1 && $nota1 < 4) {
+            $situacao = " PÉSSIMO";
+            echo " 1° Bimestre: Nota $nota1 - $situacao ";
         }
-        else if ($nota >= 4 && $nota < 6) {
+        else if ($nota1 >= 4 && $nota1 < 6) {
             $situacao = " RUIM";
-            $mensagem = "Sua nota é" . $situacao;
+            echo " 1° Bimestre: Nota $nota1 - $situacao ";
         }
-        else if ($nota >= 6 && $nota < 8) {
+        else if ($nota1 >= 6 && $nota1 < 8) {
             $situacao = " BOA";
-            $mensagem = "Sua nota é" . $situacao;
+            echo " 1° Bimestre: Nota $nota1 - $situacao ";
         }
-        else if ($nota >= 8 && $nota == 10) {
+        else if ($nota1 >= 8 && $nota1 <= 10) {
             $situacao = " EXCELENTE";
-            $mensagem = "Sua nota é" . $situacao;
+            echo " 1° Bimestre: Nota $nota1 - $situacao ";
         }
         else {
-            echo "Sua nota é invalida";
+            echo "Nota inválida";
         }
+        
+        echo "<br>";
+        echo "<br>";
+        
+        if ($nota2 >= 1 && $nota2 < 4) {
+            $situacao = " PÉSSIMO";
+            echo " 2° Bimestre: Nota $nota2 - $situacao ";
+        }
+        else if ($nota2 >= 4 && $nota2 < 6) {
+            $situacao = " RUIM";
+            echo " 2° Bimestre: Nota $nota2 - $situacao ";
+        }
+        else if ($nota2 >= 6 && $nota2 < 8) {
+            $situacao = " BOA";
+            echo " 2° Bimestre: Nota $nota2 - $situacao ";
+        }
+        else if ($nota2 >= 8 && $nota2 <= 10) {
+            $situacao = " EXCELENTE";
+            echo " 2° Bimestre: Nota $nota2 - $situacao ";
+        }
+        else {
+            echo "Nota inválida";
+        }
+        
+        echo "<br>";
+        echo "<br>";
+        
+        if ($nota3 >= 1 && $nota3 < 4) {
+            $situacao = " PÉSSIMO";
+            echo " 3° Bimestre: Nota $nota3 - $situacao ";
+        }
+        else if ($nota3 >= 4 && $nota3 < 6) {
+            $situacao = " RUIM";
+            echo " 3° Bimestre: Nota $nota3 - $situacao ";
+        }
+        else if ($nota3 >= 6 && $nota3 < 8) {
+            $situacao = " BOA";
+            echo " 3° Bimestre: Nota $nota3 - $situacao ";
+        }
+        else if ($nota3 >= 8 && $nota3 <= 10) {
+            $situacao = " EXCELENTE";
+            echo " 3° Bimestre: Nota $nota3 - $situacao ";
+        }
+        else {
+            echo "Nota inválida";
+        }
+        
+        echo "<br>";
+        echo "<br>";
+        
+        if ($nota4 >= 1 && $nota4 < 4) {
+            $situacao = " PÉSSIMO";
+            echo " 4° Bimestre: Nota $nota4 - $situacao ";
+        }
+        else if ($nota4 >= 4 && $nota4 < 6) {
+            $situacao = " RUIM";
+            echo " 4° Bimestre: Nota $nota4 - $situacao ";
+        }
+        else if ($nota4 >= 6 && $nota4 < 8) {
+            $situacao = " BOA";
+            echo " 4° Bimestre: Nota $nota4 - $situacao ";
+        }
+        else if ($nota4 >= 8 && $nota4 <= 10) {
+            $situacao = " EXCELENTE";
+            echo " 4° Bimestre: Nota $nota4 - $situacao ";
+        }
+        else {
+            echo "Nota inválida";
+        }
+        
+        echo "<br>";
+        echo "<br>";
 
-        echo "Sua nota é $nota" . " e sua situação é $situacao";
-        ?>
-</body>
-</html>
+        $media = ($nota1 + $nota2 + $nota3 + $nota4) / 4;
+        echo "Média: $media";
+        
+        echo "<br>";
+        
+        $porcentagem = ($media / 10) *100;
+        echo " Sua porcentagem de aproveitamento foi de: $porcentagem%";
+        
+        echo "<br>";
+
+        if ($media >= 6) {
+            $aprovacao = "APROVADO";
+            echo "Parabéns, você foi $aprovacao com a porcentagem de aproveitamento de $porcentagem";
+        }
+        else {
+            $aprovacao = "REPROVADO";
+            echo "Infelizmente você foi $aprovacao por não ter atingido a média";
+        }
+        
+
+    ?>
+    </body>
+    </html>
